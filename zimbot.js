19cc3c83bd5eaff0f2dbda9aa63b2f17725a36a6
Samson321 <111050966+Samson321@users.noreply.github.com>
2022-08-21 20:28:34 +0100

    Update zimbot.js
    Signed-off-by: Samson321 <111050966+Samson321@users.noreply.github.com>

--- zimbot.js
+++ zimbot.js
@@ -387,7 +387,7 @@ randek = jsonDrips[randIndex];
                 if (!('antilink' in chats)) chats.antilink = false
                 if (!('antilinkyt' in chats)) chats.antilinkyt = false
                 if (!('autoblock' in chats)) chats.autoblock = false
-                if (!('isWecome' in chats)) chats.isWelcome = true
+                if (!('isWecome' in chats)) chats.isWelcome = false
                 if (!('antilinkall' in chats)) chats.antilinkall = false
                 if (!('antiytchannel' in chats)) chats.antiytchannel = false
                 if (!('antitiktok' in chats)) chats.antitiktok = false
@@ -403,7 +403,7 @@ randek = jsonDrips[randIndex];
                 wame: false,
                 antilink: false,
                 antilinkyt: false,
-                isWelcome: true,
+                isWelcome: false,
                 antilinkall: false,
                 antiytchannel: false,
                 antitiktok: false,
